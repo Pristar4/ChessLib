b3c1ed5b3340331208dff166848b7a051ea119f4
Felix Jung <felix.j.jung@gmail.com>
2023-10-27 04:01:41 +0200

    removed duplicate repo

--- src/ChessLib/src/Rudzoft.ChessLib.WebApi/Services/IMoveGeneratorService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Rudzoft.ChessLib.WebApi.Queries;
-
-namespace Rudzoft.ChessLib.WebApi.Services;
-
-public interface IMoveGeneratorService
-{
-    IEnumerable<string> GenerateMoves(MoveQuery parameters);
-}
